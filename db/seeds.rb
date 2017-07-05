d0854989ec2707e899bbce2e07e3d548699c4fdf
B Kleinen <kleinen@infrastructure.de>
2017-07-05 10:51:14 +0200

    some database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i = Issue.create(headline: "IMI News SS2017", released_at: "2017-07-28")
+i.blog_entries.create(title: "Sommerfest am 5.7.",teaser: "im G008")
+i.blog_entries.create(title: "Showtime am 28.7",teaser: "tolle Projekte!")
